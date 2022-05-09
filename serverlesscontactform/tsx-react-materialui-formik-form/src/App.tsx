6e83cbdfd1bbc0d7362b7ceea84c8d1dd70bd957
Christian E <christianelena92@gmail.com>
2022-05-09 12:51:55 -0500

    CREATE: AppBar and mobile responsiveness, ADD: assests dir

--- serverlesscontactform/tsx-react-materialui-formik-form/src/App.tsx
+++ serverlesscontactform/tsx-react-materialui-formik-form/src/App.tsx
@@ -1,10 +1,12 @@
 import * as React from "react";
 import './App.css'; 
 import { MyForm } from "./components/MyForm";
+import  ResponsiveAppBar from "./components/AppBar"; 
 
 const App = () => { 
   return (
     <div style={{textAlign:"center"}}>
+    <ResponsiveAppBar /> 
     <MyForm onSubmit={({email, firstName, lastName, phoneNumber}) => { 
     console.log(email, firstName, lastName, phoneNumber)
     }}
